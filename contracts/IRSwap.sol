080febc215f6398125bbf5ffcae55af94b736074
Luiz Soares <luizhamilton29@gmail.com>
2024-03-15 16:48:43 -0300

    1st version

--- contracts/IRSwap.sol
+++ contracts/IRSwap.sol
@@ -1,67 +1,97 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract IRSwap  {
-
+contract IRSwap {
     struct SWAP {
-        uint256 notional;           // total amount
-        address floatRate_Account;  // float rate address to receive funds
-        address fixedRate_Account;  // fixed rate address to receive funds
-        uint256 fixedRate;          // fixed rate
-        uint256 fee;                // contract fee
-        address assetContract;      // USDT
-        uint256 startDate;          // timestamp
+        uint256 notional; // total amount
+        address floatRate_Account; // float rate address to receive funds
+        address fixedRate_Account; // fixed rate address to receive funds
+        uint256 fixedRate; // fixed rate
+        IERC20 usdt; // USDT
+        uint256 cashFlowDate; // timestamp
     }
 
     struct RateContract {
-        uint256 notional;       // total amount
-        address owner;          // to receive funds
-        uint256 rate;           // fix rate
-        bool hasSwap;           // true if a swap was set
-        //uint256 initialPayment; // timestamp of the first swap. the others would happen after 30 days. 
+        uint256 notional; // total amount
+        address owner; // to receive funds
+        uint256 rate; // fix rate
+        bool hasSwap; // true if a swap was set
+        //uint256 initialPayment; // timestamp of the first swap. the others would happen after 30 days.
     }
-
-    mapping(uint256 => RateContract) public fixed_RateContracts;
-    mapping(uint256 => RateContract) public float_RateContracts;
 
     mapping(uint256 => SWAP) public swapContracts;
 
-    uint256 public swapFee;
-    address public feeAccount;
-    uint256 public benchmark;  // float rate set every day at 8:30 am
+    uint256 public swapFee = 1;
+    address public feeAccount = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
+    uint256 public benchmark = 2; // float rate set every day at 8:30 am
 
-    function add_fixed_RateContract(uint256 id, uint256 notional, address owner, uint256 fixedRate) external {
-        fixed_RateContracts[id] = RateContract(notional,owner,fixedRate, false);
+    uint256 public MONTH_IN_SECONDS = 10;
+
+    function createSwapContract(
+        uint256 id,
+        uint256 notional,
+        address floatRate_Account,
+        address fixedRate_Account,
+        uint256 fixedRate,
+        address usdt,
+        uint256 cashFlowDate
+    ) external {
+        swapContracts[id] = SWAP(
+            notional,
+            floatRate_Account,
+            fixedRate_Account,
+            fixedRate,
+            IERC20(usdt),
+            cashFlowDate + MONTH_IN_SECONDS
+        );
     }
 
-    function add_float_RateContract(uint256 id, uint256 notional, address owner, uint256 floateRate) external {
-        float_RateContracts[id] = RateContract(notional,owner,floateRate, false);
-    }
+    uint256 public fixedAmountTotal;
+    uint256 public floatAmountTotal;
+    uint256 public feeAmountTotal;
 
     // calculate swap. get float rate.
-    function executeSwap(uint256 id) external{
-        SWAP memory swap = swapContracts[id];
-        
+    function executeSwap(uint256 id) external {
+        SWAP storage swap = swapContracts[id];
+
         // check if swap was executed already or the time and date is valid (can be executed)
+        //require(swap.cashFlowDate < block.timestamp);
 
-        // set swap for that payment day is done
+        // can be executed again in 30 days
+        swap.cashFlowDate = block.timestamp + MONTH_IN_SECONDS;
 
-        uint256 fixedAmount = swap.notional * swap.fixedRate - swap.notional * swapFee;
-        uint256 floatAmount = swap.notional * floatRate - swap.notional * swapFee;
+        uint256 fee = ((swap.notional * swapFee) / 1000) / 2;
 
-        if(fixedAmount == floatAmount) {
-            // do nothing
-        } else {
-            // transfer fixed amount to the float account
-            
-            // transfer float amount to the fixed account
+        uint256 fixedAmount = ((swap.notional * swap.fixedRate) / 100) - fee;
 
-            // transfer fee to the fee account
-        }
- 
-        // event swap was executed
+        uint256 floatAmount = ((swap.notional * benchmark) / 100) - fee;
+        fixedAmountTotal = fixedAmount;
+        floatAmountTotal = floatAmount;
+        feeAmountTotal = fee;
+        //transferFrom(address from, address to, uint256 value) external returns (bool);
+        //fixed receives float
+        swap.usdt.transferFrom(
+            swap.floatRate_Account,
+            swap.fixedRate_Account,
+            floatAmount
+        );
+
+        // float receives fixed amount
+        swap.usdt.transferFrom(
+            swap.fixedRate_Account,
+            swap.floatRate_Account,
+            fixedAmount
+        );
+
+        // pay fee
+        swap.usdt.transferFrom(swap.fixedRate_Account, feeAccount, fee);
+        swap.usdt.transferFrom(swap.floatRate_Account, feeAccount, fee);
     }
 
+    function updateBenchMarck(uint256 newBenchmark) external {
+        benchmark = newBenchmark;
+    }
     // create function to update floatRate
-
 }
